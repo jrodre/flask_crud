a7c1872df890f91d1703cdc05295985ce24ec220
Jostin Rodriguez <jarzta99@gmail.com>
2024-09-20 00:51:13 -0500

    Cambios para subida de archivos

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask_crud',
-    version='0.0.0.1',
+    name='flaski',
+    version='0.0.0.2',
     packages=['flask_crud'],
     # packages=find_packages(),
     description='Administra vistas con un enfoque diferente en flask.',
     long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
     # long_description=open('README.md').read(),
-    author='Tu nombre',
-    author_email='jodriz@example.com',
+    author='Jodriz Dev',
+    author_email='jrodriguez7603@utm.edu.ec',
     url='https://github.com/jrodre/flask_crud.git',
     install_requires=[
     "Flask"
